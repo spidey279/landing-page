d3f7e4554c6932c499b1587bfc4a702a3e17c823
yyyaaassshh <yashpawar2995@gmail.com>
2025-07-30 02:59:35 +0530

    error fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import XIcon from "@mui/icons-material/X";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import EmailIcon from "@mui/icons-material/Email";
 
 export const Footer = () => {
   const links = [
